ff2ed2c8c0084ead40be398e05a8997516d78823
unknown <khurshidimran14@gmail.com>
2021-07-09 03:04:04 +0500

    edit,delete in menu,give id to css table,move function of date to Util.js

--- src/js/firebaseconfig.js
+++ src/js/firebaseconfig.js
@@ -1,1 +1,10 @@
 
+export default {
+    apiKey: "AIzaSyBbtdQBF01HUt51eZmUdwUJjwr29w_jaSg",
+    authDomain: "bonfire-207913.firebaseapp.com",
+    databaseURL: "https://bonfire-207913.firebaseio.com",
+    projectId: "bonfire-207913",
+    storageBucket: "bonfire-207913.appspot.com",
+    messagingSenderId: "757617452040",
+    appId: "1:757617452040:web:5f277923ce60e40bc89dbd"
+  };
