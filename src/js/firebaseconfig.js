57aac577847ae9509ed5778416c258893fc47ead
Muhammad Arshad <marshad.dev@gmail.com>
2021-07-09 09:45:58 +0000

    Update firebaseconfig.js
    Updated config with global project

--- src/js/firebaseconfig.js
+++ src/js/firebaseconfig.js
@@ -1,10 +1,10 @@
 
 export default {
-    apiKey: "AIzaSyBbtdQBF01HUt51eZmUdwUJjwr29w_jaSg",
-    authDomain: "bonfire-207913.firebaseapp.com",
-    databaseURL: "https://bonfire-207913.firebaseio.com",
-    projectId: "bonfire-207913",
-    storageBucket: "bonfire-207913.appspot.com",
-    messagingSenderId: "757617452040",
-    appId: "1:757617452040:web:5f277923ce60e40bc89dbd"
-  };
+  apiKey: "AIzaSyArhN_fUHGNRzG7x6TU9qYf3KUfgRm7ZBk",
+  authDomain: "restaurant-app-8b992.firebaseapp.com",
+  projectId: "restaurant-app-8b992",
+  storageBucket: "restaurant-app-8b992.appspot.com",
+  messagingSenderId: "178510203105",
+  appId: "1:178510203105:web:0c98b40748b0b24ccb8fa0",
+  measurementId: "G-1DV3MY80R8"
+};
