9b0e3908e4935cb43d26888095d14443d40b13b6
Abbas <abbas.thegreat5@gmail.com>
2021-07-12 16:57:33 +0500

    Admin Authentication Applied

---
+++ src/js/firebaseconfig.js
@@ -0,0 +1,9 @@
+
+export default {
+   apiKey: "AIzaSyBLpolP5IIzJxnv3iTLl7PuuGetVrIt2tI",
+    authDomain: "crud-6ff60.firebaseapp.com",
+    projectId: "crud-6ff60",
+    storageBucket: "crud-6ff60.appspot.com",
+    messagingSenderId: "901446171276",
+    appId: "1:901446171276:web:36cec09ca85e95889f19a9"
+};
---
+++ src/js/menus.js
@@ -0,0 +1,84 @@
+import App from "./App.js";
+import { createRow, updateRow, deleteRow } from "./Util.js";
+
+ (function onloadData() {
+    
+    const db = App.getdb('Menu');
+    db.renderToList((id, model, change) => {
+        if (model) {
+            if (change == 'added') {
+                createRow('#menuRow', id, model);
+            } else if (change == 'modified') {
+                updateRow(id, model);
+            } else {
+                deleteRow(id);
+            }
+        }
+    });
+
+
+    setTimeout(() => {
+        // edit menu
+        const editMenuArr = document.querySelectorAll(".editmenu");
+        for (let i of editMenuArr) {
+            i.addEventListener("click", (e) => {
+                e.preventDefault();
+                let tr = e.currentTarget.parentElement.parentElement;
+                var td = tr.querySelectorAll('td');
+                const form = document.getElementById('menuForm');
+                for (let index = 0; index < form.length; index++) {
+                    const element = form[index];
+                    if (element && element.value != "Save" && element.name != "id")
+                        element.value = td[index].textContent;
+                    if(element.name == "id")
+                            element.value = tr.dataset.id;
+                }
+            });
+        }
+        // delete menu
+        const delMenuArr = document.querySelectorAll(".delmenu");
+        for (let i of delMenuArr) {
+            i.addEventListener("click", (e) => {
+                e.preventDefault();
+                let tr = e.currentTarget.parentElement.parentElement;
+                if(db.remove(tr.dataset.id))
+                    tr.remove();
+            });
+        }
+
+      }, 3000);
+}());
+
+document.addEventListener('submit', async function(e) {
+    e.preventDefault();
+    const db = App.getdb('Menu');
+    const form = document.getElementById('menuForm');
+    var model = {};
+    var modelid;
+    for (let index = 0; index < form.length; index++) {
+        const element = form[index];
+        if (element && element.value && element.value != "Save" && element.name != "id")
+            model[element.name] = element.value;
+        if(element.name == "id" && element.value) 
+            modelid = element.value;   
+    }
+    if (model.name && model.category) {
+        var doc = await db.query('name', '==', model.name);
+        console.log(doc);
+        if (doc.size == 0 && !modelid) {
+            await db.add(model)
+                // .then(function() {
+                    console.log("Record added."); // Record will be added through our renderToList
+                // });
+            
+        }
+        else
+        {
+            await db.update(modelid,model);
+            console.log("Record updated.");
+        }
+        form.reset();
+    }
+    return false;
+});
+export default menuLoad;
